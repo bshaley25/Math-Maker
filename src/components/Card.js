1bffcfbedfb30de729cbb7483a8c44b3f2dafd75
Bshaley25 <bshaley25@gmail.com>
2020-04-17 19:06:32 -0400

    organized folder layout

---
+++ src/components/Card.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default ({ grid, loadSavedGrid, deleteGrid }) => {
+
+    const handleClick = () => {
+        const gridData =  JSON.parse(grid.data)
+        loadSavedGrid(grid.colums, grid.rows, gridData, grid.id)
+    }
+    
+    const handleDelete = (event) => {
+        event.preventDefault()
+        deleteGrid(grid.id)
+    }
+
+    return (
+        <section className='card' >
+            {grid.id}
+            <button onClick={handleClick}>Edit</button>
+            <button onClick={handleDelete}>Delete</button>
+        </section>
+    )
+}
