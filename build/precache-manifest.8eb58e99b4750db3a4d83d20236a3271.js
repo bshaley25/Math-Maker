324c61da88e20ad1339c7e7f7043bcb0f12bdbf6
Bshaley25 <bshaley25@gmail.com>
2020-07-12 22:18:53 -0400

    clean up commit

---
+++ build/precache-manifest.8eb58e99b4750db3a4d83d20236a3271.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c41c6ad9b1063ca556df7bfa1f33512",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a844ad5f94f16a557220",
+    "url": "/static/css/main.b4e71bee.chunk.css"
+  },
+  {
+    "revision": "d99e22c3b3a1a49938f5",
+    "url": "/static/js/2.451eeee0.chunk.js"
+  },
+  {
+    "revision": "a7fd87fc5548f39a5a5d533d9ad9040c",
+    "url": "/static/js/2.451eeee0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a844ad5f94f16a557220",
+    "url": "/static/js/main.097c09ad.chunk.js"
+  },
+  {
+    "revision": "ce95cf170d5749ede985",
+    "url": "/static/js/runtime-main.c62c10d2.js"
+  }
+]);
